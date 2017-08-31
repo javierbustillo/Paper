7fe7a1cc4a16df6fa51ea2b43418f0b6697a7c56
javierbustillo <javier.bustillo@upr.edu>
2017-08-30 21:00:57 -0400

    Fixed crash when app loads for the firs time on a new device

--- uprm1/AppDelegate.swift
+++ uprm1/AppDelegate.swift
@@ -24,6 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         Parse.initialize(with: configuration)
         PFUser.enableAutomaticUser()
+        PFUser.current()?.saveInBackground()
         
 
         
